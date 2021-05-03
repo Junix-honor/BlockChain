f8596f1f09f2fa19adb07cbe991ae61409bc6fc7
DavidDDDDP <m13539590365_1@163.com>
2021-05-03 19:39:32 +0800

    错误处理+余额查询

---
+++ app/common_deal/Query_Balance.py
@@ -0,0 +1,21 @@
+import web3
+from web3 import Web3, IPCProvider
+from decimal import *
+
+def Query_Balance(RPC_server,address):
+    try:
+        web3.HTTPProvider(RPC_server)
+    except ConnectionError:
+        print("Connection error")
+        return 0
+
+    w3 = Web3(Web3.HTTPProvider(RPC_server))
+    balance = w3.fromWei(w3.eth.getBalance(address), 'ether')
+    return balance
+
+
+if __name__ == "__main__":
+    RPC_server = "HTTP://127.0.0.1:7545"
+    address = "0x5971E440b0b43536640d417F1502AA4F48489055"
+    result = Query_Balance(RPC_server,address)
+    print(round(result,3),'Eth')
